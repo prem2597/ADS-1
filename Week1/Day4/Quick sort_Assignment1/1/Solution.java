e08d9b09535e1d1efcd7989bbf184d20e80b08c6
prem2597 <prembammidi25@msitprogram.net>
2019-11-02 10:36:11 +0530

    git commit

--- Week1/Day4/Quick sort_Assignment1/1/Solution.java
+++ Week1/Day4/Quick sort_Assignment1/1/Solution.java
@@ -9,16 +9,24 @@ class Solution {
 		for (int j = low; j < high; j++) {
 			if (arr[j] <= pivot) {
 				i++;
-				int temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
+				swap (arr, i, j);
+				// int temp = arr[i];
+				// arr[i] = arr[j];
+				// arr[j] = temp;
 			}
 		}
-		int temp2 = arr[i + 1];
-		arr[i + 1] = arr[high];
-		arr[high] = temp2;
+		swap (arr, i+1, high);
+		// int temp2 = arr[i + 1];
+		// arr[i + 1] = arr[high];
+		// arr[high] = temp2;
 
 		return i + 1;
+	}
+	public static void swap (final int[] arr, final int i, final int j) {
+		int temp;
+		temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
 	}
 	public static int[] quickSort(final int[] arr,
 	final int low, final int high) {
