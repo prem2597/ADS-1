b13b1dd37e9c11c6ca360c46a7977fe594629f61
prem2597 <prembammidi25@msitprogram.net>
2019-11-04 16:44:29 +0530

    git commit

--- Week1/Day6/Assignment1/Minpq.java
+++ Week1/Day6/Assignment1/Minpq.java
@@ -1,10 +1,25 @@
 import java.util.*;
 
-public class Minpq<Key extends Comparable<Key>> {
+public class Minpq {
 
-    private Minpr() {
-
+    public static boolean isHeap(int arr[], int i, int n) {
+        if (i < (n - 2) / 2) {
+            return true;
+        }
+        if(arr[i] <= arr[2 * i + 1] && arr[i] <= arr[2 * i + 2]
+        && isHeap(arr, 2 * i + 1, n) && isHeap(arr, 2 * i + 2, n)) {
+            return true;
+        }
+        return false;
     }
-
     
+    public static void main(String[] args) {
+        int arr[] = {1000, 90, 15, 10, 7, 12, 100, 7, 3};
+        int n = arr.length - 1;
+        if (isHeap(arr, 0, n)) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }
 }
