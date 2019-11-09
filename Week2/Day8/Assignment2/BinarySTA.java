07d316f2150285a5285f0b9b380e148560ae2717
prem2597 <prembammidi25@msitprogram.net>
2019-11-10 00:14:23 +0530

    git commit

--- Week2/Day8/Assignment2/BinarySTA.java
+++ Week2/Day8/Assignment2/BinarySTA.java
@@ -1,92 +1,84 @@
 import java.util.Arrays;
-
-public class BinarySTA<Key extends Comparable<Key>, Value> {
+/**
+ * @author PREM.
+ * @param <Key> key
+ * @param <Value> value
+ */
+public class BinarySTA<Key extends Comparable<Key>,Value> {
     Key[] keys;
-    Value[] values;
-	int[] leftLinks,rightLinks,size;
-	private final int sizeLimit = 1000;
-	int count;
-
-	BinarySTA () {
+    Value[] val;
+    int[] leftLinks;
+    int[] rightLinks;
+    int[] sizes;
+    private final int sizeLimit = 1000;
+    int count;
+    BinarySTA() {
         keys = (Key[]) new Comparable[sizeLimit];
-        values = (Value[]) new Object[sizeLimit];
+        val = (Value[]) new Object[sizeLimit];
         leftLinks = new int[sizeLimit];
         rightLinks = new int[sizeLimit];
-
-		size = new int[sizeLimit];
-		count = 0;
-	}
-
-	public void put(Key key, Value value) {
-		if (key == null || value == null) {
-	        throw new IllegalArgumentException();
-	    }
-		if (size() == keys.length) {
-			System.out.println("Size Limit Reached.");
-			return;
-		}
-		put(0, key, value);
-	}
-	
-	private void put(int index, Key key, Value value) {
-		if (index == -1 || keys[index] == null) {
-			int nextElementIndex = size();
-			keys[nextElementIndex] = key;
-			values[nextElementIndex] = value;
-	        size[nextElementIndex] = 1;
-
-			return nextElementIndex;
-		}    
-	
-		int compare = key.compareTo(keys[index]);
-	
-		if (compare < 0) {
-			leftLinks[index] = put(leftLinks[index], key, value);
-		} else if (compare > 0) {
-			rightLinks[index] = put(rightLinks[index], key, value);
-		} else {
-			values[index] = value;
-		}
-	
-		size[index] = size(leftLinks[index]) + 1 + size(rightLinks[index]);
-		return;
-	}
-
-	public void display() {
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i] != null) {
-                System.out.println("(" + i + "-" + keys[i] + "," + values[i] + ")" + "-" + leftLinks[i] + "-" + rightLinks[i]);
-            }
-        }
-	}
-	
-	public void resize() {
-        keys = Arrays.copyOf(keys, 2 * keys.length);
-        values = Arrays.copyOf(values, 2 * values.length);
-        leftLinks = Arrays.copyOf(leftLinks, 2 * leftLinks.length);
-        rightLinks = Arrays.copyOf(rightLinks, 2 * leftLinks.length);
-        size = Arrays.copyOf(size, 2 * size.length);
-	}
-
-    public Value get(Key newKey) {
-        if (newKey == null){
-            throw new IllegalArgumentException("argument passed is null");
-        }
-        return get(1, newKey);
+        sizes = new int[sizeLimit];
+        count = 0;
     }
 
-    private Value get(int index, Key newKey) {
+    public void put(Key k, Value v) {
+        if (k == null) {
+            throw new IllegalArgumentException("key cannot be empty");
+        }
+        // if (sizes == keys.length) {
+        //     System.out.println("Size Limit Reached.");
+        //     return;
+        // }
+        put(1, k, v);
+    }
+
+    private void put(int index, Key k, Value v) {
+        if (index > keys.length - 1) {
+            // resize();
+            keys[index] = k;
+            val[index] = v;
+            sizes[index] = 1;
+            return ;
+        }
+        if (keys[index] == null) {
+            keys[index] = k;
+            val[index] = v;
+            return;
+        }
+        int cmp = k.compareTo(keys[index]);
+        if (cmp < 0) {
+            leftLinks[index] = 2 * index;
+            put(2 * index, k, v);
+        } else if (cmp > 0) {
+            rightLinks[index] = (2 * index) + 1;
+            put((2 * index) + 1, k, v);
+        } else {
+            val[index] = v;
+        }
+
+        sizes[index] = 1 + sizes[2 * index] + sizes[(2 * index) + 1];
+        return;
+    }
+
+    public Value get(Key k) {
+        if (k == null){
+            throw new IllegalArgumentException("argument passed is null");
+        }
+        return get(1, k);
+    }
+
+    private Value get(int index, Key k) {
         if (keys[index] == null) {
             return null;
         }
-        int cmp = newKey.compareTo(keys[index]);
+        int cmp = k.compareTo(keys[index]);
         
         if (cmp < 0) {
-            return get(2 * index, newKey);
+            return get(2 * index, k);
         } else if (cmp > 0) {
-            return get(1 + (2 * index),newKey);
+            return get(1 + (2 * index),k);
         } else {
-            return values[index];
+            return val[index];
         }
     }
 }
--- Week2/Day8/Assignment2/Main.java
+++ Week2/Day8/Assignment2/Main.java
@@ -6,22 +6,14 @@ public class Main {
         obj.put("G", 70);
         obj.put("O", 21);
         obj.put("R", 542);
-        obj.put("I", 15);
-        obj.put("T", 70);
-        obj.put("H", 99);
-        obj.put("M", 119);
         obj.put("E", 569);
         obj.put("P", 909);
         obj.put("L", 133);
         obj.put("E", 334);
-        obj.display();
-
-
         System.out.println(obj.get("R"));
         System.out.println(obj.get("A"));
         System.out.println(obj.get("E"));
         System.out.println(obj.get("G"));
-        System.out.println(obj.get("I"));
 
     }
 }
