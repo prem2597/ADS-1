63210fc9f04f8b3d871c15e1f1f3f2951eb9d2cf
prem2597 <prembammidi25@msitprogram.net>
2019-11-14 18:36:42 +0530

    git commit

--- assignment/a1/FarthestPair.java
+++ assignment/a1/FarthestPair.java
@@ -1,6 +1,7 @@
 // import java.util.Arrays;
 import java.util.Scanner;
 // import java.util.Random;
+// import java.lang.IndexOutOfBoundsException;
 /**
  * @author PREM
  */
@@ -12,22 +13,12 @@ public final class FarthestPair {
 
     }
     /**
-     * Main method.
-     * @param args arguments
+     * fathest methods tells the min value and the max value
+     * @param arr array
+     * @param min minimum value
+     * @param max maximum value
      */
-    public static void main(final String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the size of the array");
-        int n = scan.nextInt();
-        // Random rand = new Random(n);
-        // int[] arr = {10, 5, 40, 52, 42, 100, 65};
-        int[] arr = new int[n];
-        int max;
-        int min;
-        System.out.println("Enter the elements into the array");
-        for (int i = 0; i < n; i++) {
-            arr[i] = scan.nextInt();
-        }
+    public static void farthest(final int[] arr, int min, int max) {
         int m = arr.length;
         if (m == 1) {
             max = arr[0];
@@ -43,8 +34,8 @@ public final class FarthestPair {
                 min = arr[0];
             }
         }
-        max = arr[0];
-        min = arr[0];
+        // max = arr[0];
+        // min = arr[0];
         for (int i = 0; i <= m - 1; i++) {
             if (arr[i] > max) {
                 max = arr[i];
@@ -54,12 +45,37 @@ public final class FarthestPair {
                 min = arr[i];
             }
         }
-        // System.out.println("Sorted Array :");
-        // Arrays.sort(arr);
+        // to print the array.
         for (int i = 0; i <= m - 1; i++) {
             System.out.print(arr[i] + " ");
         }
-        scan.close();
         System.out.println("Farthest Pair :" + min + " " + max);
+
+    }
+    /**
+     * Main method.
+     * @param args arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // System.out.println("Enter the size of the array");
+        // int n = scan.nextInt();
+        // if (n < 0) {
+        //     throw new IndexOutOfBoundsException("Array index out of bound");
+        // }
+        // Random rand = new Random(n);
+        // int[] arr = {10, 5, 40, 52, 42, 100, 65};
+        int n = 10;
+        int[] arr = new int[n];
+        System.out.println("Enter the elements into the array");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+        }
+        int max = arr[0];
+        int min = arr[0];
+        farthest(arr, min, max);
+        // System.out.println("Sorted Array :");
+        // Arrays.sort(arr);
+        scan.close();
     }
 }
