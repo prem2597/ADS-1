61c6e2d7358f65b857a747d28c23962518b826e9
prem2597 <prembammidi25@msitprogram.net>
2019-11-19 15:39:39 +0530

    git commit

---
+++ assignment/A5/BinarySearchST.java
@@ -0,0 +1,97 @@
+import java.util.*;
+public class BinarySearchST<Key extends Comparable, Value> {
+    private Key[] keys;
+    private Value[] vals;
+    private int n = 0;
+    int range = 10;
+    public BinarySearchST() {
+        keys = (Key[]) new Comparable[range];
+        vals = (Value[]) new Object[range];
+    }
+    private void resize(int range) {
+        Key[] x = (Key[]) new Comparable[range];
+        Value[] y = (Value[]) new Object[range];
+        for (int i = 0; i < n; i++) {
+            x[i] = keys[i];
+            y[i] = vals[i];
+        }
+        vals = y;
+        keys = x;
+    }
+    public int size() {
+        return n;
+    }
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+    public boolean contains(Key key) {
+        return get(key) != null;
+    }
+    public Value get(Key key) {
+        if(isEmpty()) {
+            return null;
+        }
+        int i = rank(key);
+        if(i < n && keys[i].compareTo(key) == 0) {
+            return vals[i];
+        }
+        return null;
+    }
+    public int rank(Key key) {
+        int lo = 0;
+        int hi = n-1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            int cmp = key.compareTo(keys[mid]);
+            if (cmp < 0) {
+                hi = mid - 1;
+            } else if (cmp > 0) {
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return lo;
+    }
+    public void put(Key key, Value val) {
+        if (n == keys.length - 1) {
+            resize(2 * keys.length);
+        }
+        if (n == 0) {
+            keys[n] = key;
+            vals[n++] = val;
+            return;
+        }
+        int m = n;
+        if (keys[m - 1].compareTo(key) > 0) {
+            keys[n] = key;
+            vals[n++] = val;
+            return;
+        }
+        int i = rank(key);
+        if(i < n && keys[i].compareTo(key) == 0) {
+            vals[i] = val;
+            return;
+        }
+        if (n == keys.length) {
+            resize(2 * keys.length);
+        }
+        for (int j = n; j > i; j--) {
+            keys[j] = keys[j - 1];
+            vals[j] = vals[j - 1];
+        }
+        keys[i] = key;
+        vals[i] = val;
+        n++;
+    }
+    public int size(Key lo, Key hi) {
+        if (lo.compareTo(hi) > 0) {
+            return 0;
+        }
+        if (contains(hi)) {
+            return rank(hi) - rank(lo) + 1;
+        } else {
+            return rank(hi) - rank(lo);
+        }
+    }
+}
---
+++ assignment/A5/Solution.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class Solution {
+    private Solution() {
+    }
+    public static void main(String[] args) {
+        BinarySearchST<String, Integer> x = new BinarySearchST<String, Integer>();
+        x.put("A",500);
+        // System.out.println(x.key);
+        // System.out.println("-----");
+        System.out.println(x.get("A"));
+        // System.out.println("-----");
+        x.put("B",1);
+        // System.out.println(x.get("B"));
+        x.put("C",2);
+        x.put("D",3);
+        x.put("E",4);
+        x.put("F",5);
+        x.put("G",6);
+        x.put("H",7);
+        System.out.println("-----");
+        System.out.println(x.get("H"));
+        System.out.println("-----");
+        x.put("I",8);
+        x.put("J",9);
+        x.put("K",10);
+        x.put("L",11);
+        x.put("M",12);
+        x.put("Z",0);
+        x.put("Y",1);
+        x.put("X",2);
+        x.put("W",3);
+        x.put("V",4);
+        x.put("U",5);
+        x.put("T",6);
+        x.put("S",7);
+        x.put("R",8);
+        x.put("Q",9);
+        x.put("P",10);
+        x.put("O",11);
+        x.put("N",12);
+        System.out.println(x.get("M"));
+        System.out.println(x.get("Z"));
+    }
+}
